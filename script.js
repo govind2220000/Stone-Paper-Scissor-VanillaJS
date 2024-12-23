c713d79090c943f6d941222cf6dadd5df44f7681
govind2220000 <govind2220000@gmail.com>
2024-12-24 02:25:16 +0530

    Added playAgain Functionality and gameElement Selector on basis of click and injecting the DOM elements on html body

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ function closeRules() {
   //showRulesValue = false;
   document.querySelector(".rule-container").style.display = "none";
 }
+
+// Add event listener to the body element
+document.body.addEventListener("click", closeRules);
+
+//Function to handle player selection
+function handleClick(imageSrc) {
+  //Update the image based on player selection
+  document.querySelector(
+    ".after-clicking .player img"
+  ).src = `./assets/${imageSrc}.png`;
+
+  //Scissors png has a different size hence we need to handle it separately
+  if (imageSrc === "scissors") {
+    document.querySelector(".after-clicking .player img").style.width =
+      "2.675rem";
+    document.querySelector(".after-clicking .player img").style.height = "3rem";
+  }
+
+  //Update the image based on computer selection
+  document.querySelector(
+    ".after-clicking .computer img"
+  ).src = `./assets/${imageSrc}.png`;
+
+  //Scissors png has a different size hence we need to handle it separately
+  if (imageSrc === "scissors") {
+    document.querySelector(".after-clicking .computer img").style.width =
+      "2.675rem";
+    document.querySelector(".after-clicking .computer img").style.height =
+      "3rem";
+  }
+
+  //Show the after-clicking spart
+  document.querySelector(".after-clicking").style.display = "flex";
+
+  //Hide the before-clicking spart
+  document.querySelector(".before-clicking").style.display = "none";
+}
+
+//Function to handle Play Again button
+function playAgain() {
+  document.querySelector(".after-clicking").style.display = "none";
+  document.querySelector(".before-clicking").style.display = "flex";
+}
